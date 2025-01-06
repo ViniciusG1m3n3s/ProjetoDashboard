60887c42d097e27131928ef451b317c8731cfe4d
ViniciusG1m3n3s <viniciusaugustogimenes@outlook.com>
2025-01-06 14:37:56 -0300

    hide streamlit brand

--- login.py
+++ login.py
@@ -98,7 +98,38 @@ def login():
     """
     st.markdown(background_image_css, unsafe_allow_html=True)
 
-    
+    hide_streamlit_style = """
+                    <style>
+                    div[data-testid="stToolbar"] {
+                    visibility: hidden;
+                    height: 0%;
+                    position: fixed;
+                    }
+                    div[data-testid="stDecoration"] {
+                    visibility: hidden;
+                    height: 0%;
+                    position: fixed;
+                    }
+                    div[data-testid="stStatusWidget"] {
+                    visibility: hidden;
+                    height: 0%;
+                    position: fixed;
+                    }
+                    #MainMenu {
+                    visibility: hidden;
+                    height: 0%;
+                    }
+                    header {
+                    visibility: hidden;
+                    height: 0%;
+                    }
+                    footer {
+                    visibility: hidden;
+                    height: 0%;
+                    }
+                    </style>
+                    """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
     
     return False  # Retorna False se o login falhar
 
